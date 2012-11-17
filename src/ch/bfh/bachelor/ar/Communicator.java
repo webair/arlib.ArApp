dcd192bd1c22d2f77902163ff90a0a097ab30051
Michael Moser <mosem7@bfh.ch>
2012-11-17 17:05:16 +0100

    imported the demoApp

---
+++ src/ch/bfh/bachelor/ar/Communicator.java
@@ -0,0 +1,112 @@
+package ch.bfh.bachelor.ar;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.StreamCorruptedException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import ch.bfh.arApp.com.GetObjects;
+import ch.bfh.arApp.com.SendObjects;
+import ch.bfh.arApp.model3D.Object3D;
+
+
+
+public class Communicator implements Runnable
+{
+	public String msg;
+	public ArrayList<Object3D> l;
+	String ip;
+	TextView t;
+	int port;
+	private Socket socket;
+	
+	public Communicator (String ip, int port)
+	{
+		this.t = t;
+		this.ip=ip;
+		this.port=port;
+	}
+	
+	private void sendObject(Serializable s)
+	{
+		ObjectOutputStream oos =null;
+		try {
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			oos.writeObject(s);
+			oos.flush();
+		} catch (IOException e) {
+			System.out.println("CLIENTFEHLER!");
+			e.printStackTrace();
+		}
+	}
+	private void getObject()
+	{
+		
+		ObjectInputStream ois;
+		try {
+			ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+			Object o = ois.readObject();
+			if(o instanceof String)
+			{
+				msg = (String) o;
+			}
+			if(o instanceof Object3D)
+			{
+				msg = "it's an Model3D";
+			}
+			if(o instanceof SendObjects)
+			{
+				
+				l = ((SendObjects) o).getModel3D();
+				msg = "it's an SendObjects!: "+l.get(0).northangle;
+			}
+			else
+			{
+				msg="unknown object...";
+			}
+
+				
+
+			
+		} catch (StreamCorruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public void run() {
+		
+		GetObjects go = new GetObjects(0l, 0l, 10);
+		try {
+			
+			socket = new Socket(ip, port);
+			Log.w("msg1", "request objects");
+			this.sendObject(go);
+			Log.w("msg1", "waiting for requested objects");
+			this.getObject();
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- src/ch/bfh/bachelor/ar/MainActivity.java
+++ src/ch/bfh/bachelor/ar/MainActivity.java
@@ -1,5 +1,7 @@
 package ch.bfh.bachelor.ar;
 
+import java.util.ArrayList;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
@@ -10,12 +12,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import ch.bfh.arApp.model3D.Object3D;
 import ch.bfh.bachelor.ar.opengl.CameraRenderer;
 import ch.bfh.bachelor.ar.opengl.OpenGLSurfaceView;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 	private CameraRenderer cr;
+	private  ArrayList<Object3D> l;
+	private static final boolean loadModules=true;
 	
     private BaseLoaderCallback  mOpenCVCallBack = new BaseLoaderCallback(this) {
     	@Override
@@ -45,6 +51,43 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		if(loadModules)
+		{
+		       l = null;
+		       Communicator c = new Communicator("83.169.4.84", 3131);
+		       Thread a = new Thread(c);
+		       a.start();
+		       try {
+		    	   a.join();
+		    	   l = c.l;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		       if(l!=null)
+		       {
+		    	   //Übergeben der Objekte..
+		    	   for(int i=0;i<l.size();i++)
+		    	   {
+		    		   Object3D b = l.get(i);
+		    		   short[] facesSh = new short[b.face.length];
+		    		   float[] bbox = new float[24];
+		    		   for(int q=0;q<8;q++)
+		    		   {
+		    			   for(int u=0;u<3;u++)
+		    			   {
+		    				   bbox[q*u] = b.bb[q][u];
+		    			   }
+		    		   }
+		    		   for(int j =0;j<b.face.length;j++)
+		    		   {
+		    			   facesSh[j] = (short) b.face[j];
+		    		   }
+		    					   
+		    		   ArLib.addModel(b.vnt, facesSh, b.pointOfGravity, bbox, b.northangle, b.lat, b.lon);
+		    	   }
+		       }
+		}
 		
         if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_2, this, mOpenCVCallBack))
         {
