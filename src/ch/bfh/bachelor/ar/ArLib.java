718f9c90f202b10a2a280b5ced3d8da492eecf9a
Chris Weber <chris@appswithlove.com>
2013-01-17 16:27:44 +0100

    added comments

--- src/ch/bfh/bachelor/ar/ArLib.java
+++ src/ch/bfh/bachelor/ar/ArLib.java
@@ -1,16 +1,24 @@
 package ch.bfh.bachelor.ar;
-
+/*
+	interface class to access the native c++ code
+*/
 public class ArLib {
 	static {
 		System.loadLibrary("ar-jni");
 	}
-
+	/*
+		initialize the AR Library
+	*/
 	public static native void initArLib(int viewPortWidth, int viewPortHeight,
 			int imageWith, int imageHeight, float cameraAngle
 	);
-
+	/*
+		processes the image and starts the rendering
+	*/
 	public static native void precessImage(byte[] imageData, float azimuth, float pitch, float roll, float latitude, float longitude);
-
+	/*
+		add a model to the AR Library
+	*/
 	public static native void addModel(int modelId, float[] vnt, short[] faces, 
 		float[] pointOfGravity, float[] boundingBox, float northAngle, 
 		float latitude, float longitude, byte[][] textureData, int[] materials, int[] dimension
